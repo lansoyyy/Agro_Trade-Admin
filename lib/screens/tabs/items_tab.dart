7c8a2ba0c0c78f30a4748eb3611ee13fca77c0a7
Lans12345 <olanalans12345@gmail.com>
2022-12-06 07:53:20 +0800

    wip items tab

--- lib/screens/tabs/items_tab.dart
+++ lib/screens/tabs/items_tab.dart
@@ -2,7 +2,7 @@ import 'package:agro_trade_admin/constant/colors.dart';
 import 'package:agro_trade_admin/widgets/text_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
-import 'package:syncfusion_flutter_charts/charts.dart' as fchart;
+import 'package:pie_chart/pie_chart.dart';
 
 class ItemsTab extends StatefulWidget {
   const ItemsTab({super.key});
@@ -35,6 +35,13 @@ class _AccountsTabState extends State<ItemsTab> {
       ChartData(12, 9),
     ];
 
+    Map<String, double> dataMap = {
+      "Vegetables and Fruits": 1,
+      "Poultry": 1,
+      "Meat": 1,
+      "Fish": 1,
+    };
+
     return SingleChildScrollView(
       child: Container(
         padding: const EdgeInsets.only(left: 20),
@@ -49,158 +56,91 @@ class _AccountsTabState extends State<ItemsTab> {
             const SizedBox(
               height: 20,
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Container(
-                  height: 120,
-                  width: 270,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(10.0),
-                    boxShadow: const [
-                      BoxShadow(
-                        color: Colors.black12,
-                        blurRadius: 20,
-                        spreadRadius: 5,
-                      ),
-                    ],
+            Container(
+              height: 120,
+              width: 270,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10.0),
+                boxShadow: const [
+                  BoxShadow(
+                    color: Colors.black12,
+                    blurRadius: 20,
+                    spreadRadius: 5,
                   ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                ],
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Image.asset(
+                      'assets/images/products.png',
+                      width: 50,
+                      height: 50,
+                      color: primary,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
                       children: [
-                        Image.asset(
-                          'assets/images/pending.png',
-                          width: 50,
-                          height: 50,
-                          color: primary,
-                        ),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            NormalText(
-                                label: 'Pending Trade',
-                                fontSize: 18,
-                                color: primary),
-                            const SizedBox(height: 15),
-                            BoldText(label: '1', fontSize: 42, color: primary),
-                          ],
-                        ),
+                        NormalText(
+                            label: 'Total Items Posted',
+                            fontSize: 18,
+                            color: primary),
+                        const SizedBox(height: 15),
+                        BoldText(label: '1', fontSize: 42, color: primary),
                       ],
                     ),
-                  ),
+                  ],
                 ),
-                Container(
-                  height: 120,
-                  width: 270,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(10.0),
-                    boxShadow: const [
-                      BoxShadow(
-                        color: Colors.black12,
-                        blurRadius: 20,
-                        spreadRadius: 5,
-                      ),
-                    ],
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Image.asset(
-                          'assets/images/accepted.png',
-                          width: 50,
-                          height: 50,
-                          color: primary,
-                        ),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            NormalText(
-                                label: 'Accepted Trade',
-                                fontSize: 18,
-                                color: primary),
-                            const SizedBox(height: 15),
-                            BoldText(label: '1', fontSize: 42, color: primary),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                Container(
-                  height: 120,
-                  width: 270,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(10.0),
-                    boxShadow: const [
-                      BoxShadow(
-                        color: Colors.black12,
-                        blurRadius: 20,
-                        spreadRadius: 5,
-                      ),
-                    ],
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Image.asset(
-                          'assets/images/rejected.png',
-                          width: 50,
-                          height: 50,
-                          color: primary,
-                        ),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            NormalText(
-                                label: 'Rejected Trade',
-                                fontSize: 18,
-                                color: primary),
-                            const SizedBox(height: 15),
-                            BoldText(label: '1', fontSize: 42, color: primary),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
+              ),
             ),
             const SizedBox(
               height: 30,
             ),
-            Center(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  BoldText(label: 'Monthy Trade', fontSize: 32, color: primary),
-                  const SizedBox(
-                    height: 20,
+            Container(
+              height: 400,
+              width: 500,
+              decoration: BoxDecoration(
+                  border: Border.all(color: primary, width: 2),
+                  borderRadius: BorderRadius.circular(5),
+                  color: Colors.white),
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: PieChart(
+                  dataMap: dataMap,
+                  animationDuration: const Duration(milliseconds: 800),
+                  chartLegendSpacing: 32,
+                  chartRadius: MediaQuery.of(context).size.width / 3.2,
+                  colorList: const [
+                    Colors.blue,
+                    Colors.red,
+                    Colors.green,
+                    Colors.amber
+                  ],
+                  initialAngleInDegree: 0,
+                  chartType: ChartType.disc,
+                  ringStrokeWidth: 32,
+                  centerText: "Items",
+                  legendOptions: const LegendOptions(
+                    legendPosition: LegendPosition.right,
+                    showLegendsInRow: false,
+                    showLegends: true,
+                    legendTextStyle: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                  Container(
-                      height: 300,
-                      width: 1000,
-                      decoration: BoxDecoration(
-                          border: Border.all(color: primary, width: 2),
-                          borderRadius: BorderRadius.circular(5),
-                          color: Colors.white),
-                      child:
-                          fchart.SfCartesianChart(series: <fchart.ChartSeries>[
-                        // Renders spline chart
-                        fchart.SplineSeries<ChartData, int>(
-                            dataSource: chartData,
-                            xValueMapper: (ChartData data, _) => data.x,
-                            yValueMapper: (ChartData data, _) => data.y)
-                      ])),
-                ],
+                  chartValuesOptions: const ChartValuesOptions(
+                    showChartValueBackground: true,
+                    showChartValues: true,
+                    showChartValuesInPercentage: false,
+                    showChartValuesOutside: false,
+                    decimalPlaces: 1,
+                  ),
+                  // gradientList: ---To add gradient colors---
+                  // emptyColorGradient: ---Empty Color gradient---
+                ),
               ),
             ),
             const SizedBox(
