46aa976c84ab0e3037295080a767eeeb16b985a7
Lans12345 <olanalans12345@gmail.com>
2022-12-06 08:10:07 +0800

    injected firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
 import 'package:agro_trade_admin/screens/home_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      options: const FirebaseOptions(
+    apiKey: "AIzaSyB_ODLsMzG0iBeajUF95AtM6XmkhszIs1I",
+    appId: "1:1013156330053:web:c98577c5d2f42648ac012c",
+    messagingSenderId: "1013156330053",
+    projectId: "agrotrade-69e23",
+  ));
   runApp(const MyApp());
 }
 
